6e407450192d3642db628a572c2eb64f349646ea
Vishal Takawane <vishaltakawane@Vishals-MacBook-Air.local>
2024-11-13 12:48:18 +0530

    CSS Fix of Left & Right Box

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,5 @@
 import React, { useEffect, useState } from 'react';
 import Navbar from './components/Navbar';
-import LeftBox from './components/LeftBox'; // Import LeftBox
-import RightBox from './components/RightBox'; // Import RightBox
 import './App.css';
 
 function App() {
@@ -18,10 +16,21 @@ function App() {
     <div className="App">
       <Navbar />
       <div className="main-content">
-        <div className="boxes">
-          <LeftBox />
-          <RightBox />
+        <div className="boxes-container">
+          <div className="left-box">
+            <div className="box-content">
+              <h2 className="box-title">Upload Image</h2>
+              <p className="drag-text">Drag and drop your image here</p>
+            </div>
+          </div>
+          
+          <div className="right-box">
+            <div className="box-content">
+              <h2 className="box-title">Predictions and Analysis of the Disease</h2>
+            </div>
+          </div>
         </div>
+
         <div className="message-container">
           <h1>{message}</h1>
         </div>
