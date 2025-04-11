bc96bd8abcbbce2b5a0c55d4f04f4a930dcf130a
Mac Rushi <rushikesh10kolge@gmail.com>
2025-04-11 18:29:53 +0530

    latest save

---
+++ frontend/src/app/chat/page.jsx
@@ -0,0 +1,10 @@
+export default function HomePage() {
+    return (
+      <main className="flex min-h-screen items-center justify-center bg-gray-100">
+        <h1 className="text-3xl font-bold text-gray-800">
+          Hello, Next.js 13 App Router!
+        </h1>
+      </main>
+    );
+  }
+  
